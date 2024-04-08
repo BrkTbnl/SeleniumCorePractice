1daf73f476f8062b8c0ae40b23d74069317a753a
BurakTabanli <b.tabanli93@hotmail.com>
2024-04-09 01:25:36 +0200

    Selenium Navigate manage methods

--- src/Part2/C01_WebDriverTest.java
+++ src/Part2/C01_WebDriverTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class C01_WebDriverTest {
     public static void main(String[] args) {
