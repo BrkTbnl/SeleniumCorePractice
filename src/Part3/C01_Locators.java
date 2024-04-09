43d5b94846ca656a8b4db0ea9b1a0073ff5d4dd5
BurakTabanli <b.tabanli93@hotmail.com>
2024-04-09 23:28:12 +0200

    Part3 done. Locators,Methods for webelement

---
+++ src/Part3/C01_Locators.java
@@ -0,0 +1,49 @@
+package Part3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_Locators {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        // locator with id gh-btn => all locators should be unique
+
+        //should assign the locators on webelement class
+        WebElement searchButton = driver.findElement(By.id("gh-btn"));
+        // i created locator for the search button on the main page
+        // now i can click on the button via this WebElement
+        // i can test is this button visible or not
+        // If the driver cannot find the object with findElement(),
+        // it throws a NoSuchElementException
+
+        // Search Text box on the ebay main page
+        WebElement searchTextBox = driver.findElement(By.name("_nkw"));
+        //can send test data (text) with locator
+        searchTextBox.sendKeys("Galaxy AI");
+
+        // click on the search button with click() method
+        searchButton.click();
+
+        // Generally it is not recommended to find locator with class name
+        // because class can be a name of group
+        // This approach is not acceptable during the test
+        List<WebElement> listClassLocators = driver.findElements(By.className("gh-p"));
+
+        for (int i = 0; i < listClassLocators.size(); i++) {
+            System.out.println(listClassLocators);
+
+        }
+
+        // Locator with linkText
+        WebElement linkTextEbayLocator = driver.findElement(By.linkText("https://www.ebay.com/sch/i.html?_nkw=galaxy+ai&_sop=12"));
+        linkTextEbayLocator.click();
+    }
+}
---
+++ src/Part3/C03_FacebookTest.java
@@ -0,0 +1,37 @@
+package Part3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_FacebookTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+        //to handle all cookies button
+        WebElement cookiesButton = driver.findElement(By.xpath("(//button[@value='1'])[3]"));
+        cookiesButton.click();
+
+        // Click on the create new account button
+        WebElement createNewButton = driver.findElement(By.xpath("(//a[@role='button'])[2]"));
+        createNewButton.click();
+        // Find the locator of First Name text box and send a name
+        WebElement firstNameTextBox = driver.findElement(By.xpath("//input[@name='firstname']"));
+        firstNameTextBox.sendKeys("James");
+        // Find the locator of Last Name text box and send a surname
+        WebElement lastNameTextBox = driver.findElement(By.xpath("//input[@name='lastname']"));
+        lastNameTextBox.sendKeys("Lars");
+        // Find the locator of Mobile number or email text box  and send an email
+        WebElement mobilePhoneTextBox = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        mobilePhoneTextBox.sendKeys("+123123123");
+        // Find the locator of New Password text box and send a password
+        WebElement passwordTextBox = driver.findElement(By.xpath("//input[@autocomplete='new-password']")) ;
+        passwordTextBox.sendKeys("xxxxxx");
+    }
+}
